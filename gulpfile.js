44403fd2a0716ceddffc1362745cdf29c4e809d2
ramoona <lindarian12@gmail.com>
2016-03-16 00:38:02 +0300

    add js uglifier

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,12 @@ var banksDB = require('postcss-banks-db');
 var contrast = require('postcss-contrast');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
 
 var paths = {
   scripts: ['./src/*.js'],
-  css: './src/*.css'
+  css: './src/*.css',
+  dest: './assets'
 };
 
 gulp.task('postcss', function () {
@@ -23,17 +25,30 @@ gulp.task('postcss', function () {
   ];
   return gulp.src(paths.css)
     .pipe(postcss(processors))
-    .pipe(gulp.dest('./assets'));
+    .pipe(gulp.dest(paths.dest));
 });
 
 gulp.task('browserify', function () {
   return browserify({ entries: ['./src/index.js'] })
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('assets'));
+    .pipe(gulp.dest(paths.dest));
 });
 
+gulp.task('uglify', function() {
+  return gulp.src('assets/bundle.js')
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('script', ['browserify'], function() {
+  return gulp.src('assets/bundle.js')
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dest));
+});
+
+
 gulp.task('watch', function () {
-  gulp.watch(paths.scripts, ['browserify']);
+  gulp.watch(paths.scripts, ['script']);
   gulp.watch(paths.css, ['postcss']);
 });
