bfe3654c6f4b0292ae5ec74fc6aa147ac4acee67
ramoona <lindarian12@gmail.com>
2017-07-16 22:31:11 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,45 +1,44 @@
-'use strict';
+
 
 var banksDB = require('banks-db');
 var masker = require('vanilla-masker');
 
-window.onload = function () {
-  var cardNumber = document.getElementById('number');
-  masker(cardNumber).maskPattern("9999 9999 9999 9999 99");
+var cardNumber = document.getElementById('number');
+var bankInfo = document.getElementById('card');
+var bankName = document.getElementById('bank-name');
+var cardType = document.getElementById('type');
+var hint = document.getElementById('hint');
+var userCountry = navigator.language.replace(/[a-z]*-/, '').toLowerCase();
 
-  cardNumber.oninput = function () {
-    var bank = banksDB(cardNumber.value);
-    var bankInfo = document.getElementById('card');
-    var bankName = document.getElementById('bank-name');
-    var cardType = document.getElementById('type');
-    var hint = document.querySelector('.hint');
-    var userCountry = navigator.language.replace(/[a-z]*-/, '').toLowerCase();
+masker(cardNumber).maskPattern('9999 9999 9999 9999 99');
 
-    if ( bank.code ) {
-      hint.classList.remove('visible');
-      bankInfo.classList.add('is-' + bank.code);
-      bankName.innerHTML = bank.country === userCountry ? bank.localTitle : bank.engTitle;
+cardNumber.addEventListener('input', function() {
+  var bank = banksDB(cardNumber.value);
+
+  if (bank.code) {
+    hint.classList.remove('visible');
+    bankInfo.classList.add('is-' + bank.code);
+    bankName.innerHTML = bank.country === userCountry ? bank.localTitle : bank.engTitle;
+    bankName.classList.add('visible');
+  } else {
+    bankName.classList.remove('visible');
+    bankInfo.setAttribute('class', 'card');
+    bankName.innerHTML = '';
+
+    if (cardNumber.value.length >= 7) {
+      bankName.innerHTML = 'Unknown bank';
       bankName.classList.add('visible');
+      hint.classList.add('visible');
     } else {
-      bankName.classList.remove('visible');
-      bankInfo.setAttribute('class', 'card');
-      bankName.innerHTML = '';
-
-      if (cardNumber.value.length >= 7) {
-        bankName.innerHTML = 'Unknown bank';
-        bankName.classList.add('visible');
-        hint.classList.add('visible');
-      } else {
-        hint.classList.remove('visible');
-      }
+      hint.classList.remove('visible');
     }
+  }
 
-    if ( bank.type ) {
-      cardType.innerHTML = bank.type;
-      cardType.classList.add('visible');
-    } else {
-      cardType.innerHTML = '';
-      cardType.classList.remove('visible');
-    }
-  };
-};
+  if (bank.type) {
+    cardType.innerHTML = bank.type;
+    cardType.classList.add('visible');
+  } else {
+    cardType.innerHTML = '';
+    cardType.classList.remove('visible');
+  }
+});
