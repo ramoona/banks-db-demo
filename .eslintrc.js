20d6f2a84a45d72d63abeac668901ac80077cb0d
ramoona <lindarian12@gmail.com>
2021-02-19 12:29:36 +0300

    upddate eslint config & gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/*': 0,
+      },
+    },
+  ],
+}
